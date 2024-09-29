b180f521cb08bf1dd44b4f5784a8a61c562d30ef
BilivityaOlena <bilivity@gmail.com>
2024-09-29 15:13:04 +0300

    BilivityaLW2

---
+++ ЛР 2/SQLQuery6 БІЛІВІТЯ Алена.sql
@@ -0,0 +1,82 @@
+
+CREATE DATABASE STUDENTS3;
+
+
+USE STUDENTS;
+
+ CREATE TABLE PersonalInfo3 (
+        StudentID INT PRIMARY KEY,
+        FirstName NVARCHAR(50),
+        LastName NVARCHAR(50),
+        DateOfBirth DATE
+    );
+
+
+
+
+      CREATE TABLE AcademicInfo3 (
+        RecordID INT PRIMARY KEY,
+        StudentID INT FOREIGN KEY REFERENCES PersonalInfo(StudentID),
+        Faculty NVARCHAR(100),
+        Curator NVARCHAR(100),
+        EnrollmentYear INT
+    );
+
+
+
+
+    CREATE TABLE ContactInfo3 (
+        ContactID INT PRIMARY KEY,
+        StudentID INT FOREIGN KEY REFERENCES PersonalInfo(StudentID),
+        Email NVARCHAR(100),
+        PhoneNumber NVARCHAR(15)
+    );
+
+
+
+        ALTER TABLE ContactInfo3 ADD Address NVARCHAR(200);
+
+
+
+
+    ALTER TABLE ContactInfo3 DROP COLUMN PhoneNumber;
+
+
+
+      EXEC sp_rename 'ContactInfo3.Email', 'EmailAddress', 'COLUMN';
+
+
+
+   ALTER TABLE AcademicInfo
+   ALTER COLUMN Faculty NVARCHAR(150);
+
+
+           CREATE TABLE Extracurricular (
+        ActivityID INT PRIMARY KEY,
+        StudentID INT FOREIGN KEY REFERENCES PersonalInfo(StudentID),
+        ActivityName NVARCHAR(100),
+        JoinDate DATE
+    );
+
+
+
+	DROP TABLE Extracurricular;
+
+
+
+	CREATE DATABASE TEACHER;
+
+
+	DROP DATABASE TEACHER;
+
+
+
+	USE STUDENTS;
+
+
+
+	INSERT INTO PersonalInfo (StudentID, FirstName, LastName, DateOfBirth)
+VALUES 
+    (1, 'Олександр', 'Петров', '2000-05-15'),
+    (2, 'Марія', 'Іваненко', '2001-03-22'),
+    (3, 'Василь', 'Коваленко', '1999-10-10');
