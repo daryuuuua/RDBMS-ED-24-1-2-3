e6199add14e7b117ce09cdb203094ac4af740ffe
SlakvaS <slakvasergey7@gmail.com>
2024-10-12 18:56:28 +0300

    Update ЛР2 Слаква.sql

--- ЛР 2/ЛР2 Слаква.sql
+++ ЛР 2/ЛР2 Слаква.sql
@@ -4,9 +4,9 @@ CREATE DATABASE Students;
         FirstName NVARCHAR(50),
         LastName NVARCHAR(50),
         DateOfBirth DATE )
-		INSERT INTO PersonalInfo VALUES(1, 'Олександр', 'Петров', '2000-05-15')
-	    INSERT INTO PersonalInfo VALUES(2, 'Марія', 'Іваненко', '2001-03-22')
-	    INSERT INTO PersonalInfo VALUES(3, 'Василь', 'Коваленко', '1999-10-10');
+		INSERT INTO PersonalInfo VALUES(1, 'РћР»РµРєСЃР°РЅРґСЂ', 'РџРµС‚СЂРѕРІ', '2000-05-15')
+	    INSERT INTO PersonalInfo VALUES(2, 'РњР°СЂС–СЏ', 'Р†РІР°РЅРµРЅРєРѕ', '2001-03-22')
+	    INSERT INTO PersonalInfo VALUES(3, 'Р’Р°СЃРёР»СЊ', 'РљРѕРІР°Р»РµРЅРєРѕ', '1999-10-10');
 	CREATE TABLE AcademicInfo (
         RecordID INT PRIMARY KEY,
         StudentID INT FOREIGN KEY REFERENCES PersonalInfo(StudentID),
